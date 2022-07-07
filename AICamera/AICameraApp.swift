289e1fac591a9b38877d8c2192129c4aa20dae90
kazukiomori <kazuki1110170501@gmail.com>
2022-07-07 23:49:21 +0900

    Initial Commit

---
+++ AICamera/AICameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICameraApp.swift
+//  AICamera
+//
+//  Created by Kazuki Omori on 2022/07/07.
+//
+
+import SwiftUI
+
+@main
+struct AICameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AICamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AICamera
+//
+//  Created by Kazuki Omori on 2022/07/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
