04cd61fcd7afa4369f38cf1993cec5f983fe637f
kazukiomori <kazuki1110170501@gmail.com>
2022-07-09 19:13:59 +0900

    AIの組み込み

--- AICamera/ContentView.swift
+++ AICamera/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Vision
 
 struct ContentView: View {
     @State private var image:UIImage?
@@ -24,12 +25,42 @@ struct ContentView: View {
                     .scaledToFit()
                     .frame(width: 100, height: 100, alignment: .center)
             })
-            Button(action: {}, label: {
+            Button(action:{
+                    if image != nil {
+                        imageAnalizer(image: image!)
+                    }
+                }, label: {
                 Text("AIの解析")
             })
         }
         .sheet(isPresented: $showCameraView) {
             ImagePickerSwiftUI(image: $image, showCameraView: $showCameraView)
+        }
+    }
+    func imageAnalizer(image:UIImage) {
+        let url = Bundle.main.url(forResource: "MobileNetV2FP16", withExtension: "mlmodelc")!
+        
+        let model = try! MLModel(contentsOf: url)
+        let coreMLModel = try! VNCoreMLModel(for: model)
+        
+        let request = VNCoreMLRequest(model: coreMLModel) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else { return }
+            
+            for result in results[0...4] {
+                print(result.confidence * 100, result.identifier)
+            }
+        }
+        
+        request.imageCropAndScaleOption = .centerCrop
+        
+        let ciImage = CIImage(image: image)!
+        
+        let handler = VNImageRequestHandler(cgImage: ciImage as! CGImage)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
         }
     }
 }
